5c83de46ee0eee398c13f7e3466872df5d9cccbb
mpkkii <mpkkii@mail.ru>
2022-04-19 22:35:09 +0300

    test from position and drow circle and line on map

--- Lambda.py
+++ Lambda.py
@@ -3,23 +3,41 @@ from cmath import sqrt
 from turtle import fillcolor
 import folium
 from PIL import Image
-from math import sqrt
+from math import sqrt, cos, sin, radians
+from folium.plugins import SemiCircle
 
 
 
-geo = [56.636720806323076, 47.83301060816586]
-geo_objekt = [56.62692835, 47.94006479575362]
-sunRiseGeo = [56.61795653688834, 47.76909006818436]
-sunSetGeo = [56.63448190718452, 47.952252753240984]
-sunDayHigh= [56.64713072839149, 47.869168648679214]
+geo = [56.62468857, 47.89476849]
+azimut = 180
+head = 45
+distance = 0.45091523185115
+sunRiseAzimut = 67.22
+sunSetAzimut = 292.31
+
+# geo_objekt = [56.62692835, 47.94006479575362]
+# sunRiseGeo = [56.61795653688834, 47.76909006818436]
+# sunSetGeo = [56.63448190718452, 47.952252753240984]
+# sunDayHigh= [56.64713072839149, 47.869168648679214]
 m = folium.Map(location= geo, zoom_start=12,)
 
-
+"""получение радиуса горионта"""
 def radius_gorizont():
-    d = 3.57 * sqrt(1.75)
-    r = d/2
-    print(r)
-    return r
+    d = 3.57 * sqrt(1.70) *1000
+    #r = d/2*1000
+    print(d)
+    return float(d)
+
+
+def geo_objekt(geo,azimut):
+    geo_objekt = []
+    end_lat = geo[0] + 0.009364 * radius_gorizont()/1000 * cos(radians(azimut))
+    end_lon = geo[1] + 0.009364 * radius_gorizont()/1000 * sin(radians(azimut))
+    geo_objekt.append(end_lat)
+    geo_objekt.append(end_lon)
+    print(geo_objekt)
+    return geo_objekt
+
 
 """Рисуем на карте точку наблюдения (нашу геопозицию)"""
 folium.CircleMarker(
@@ -32,23 +50,19 @@ folium.CircleMarker(
 ).add_to(m)
 
 
-"""Рисуем на карте искомую точку"""
-folium.CircleMarker(
-    location=geo_objekt,
-    radius=6,
-    popup="Искомая точка",
-    color="red",
-    fill=True,
-    fillcolor="red"
-).add_to(m)
+# """Рисуем на карте искомую точку"""
+# folium.CircleMarker(
+#     location=geo_objekt,
+#     radius=6,
+#     popup="Искомая точка",
+#     color="red",
+#     fill=True,
+#     fillcolor="red"
+# ).add_to(m)
 
 """Круг видимости горизонта"""
-folium.Circle(
-    radius=radius_gorizont()*1700,
-    location=geo,
-    color="red",
-    fill=False,
-).add_to(m)
+#folium.plugins.SemiCircle(location=geo, radius=radius_gorizont(), direction=azimut, arc=1 , color= "green" ).add_to(m)
+folium.plugins.SemiCircle(location=geo, radius=radius_gorizont(), start_angle=sunRiseAzimut, stop_angle=sunSetAzimut , color= "red" ).add_to(m)
 
 """круг хз чего"""
 folium.Circle(
@@ -60,9 +74,8 @@ folium.Circle(
 
 # folium.Polygon(locations=[sunRiseGeo, sunDayHigh, sunSetGeo]).add_to(m)
 
-
-"""линия на искомую точку и сама искомая точка"""
-folium.PolyLine([geo, geo_objekt]).add_to(m)
+# """линия на искомую точку и сама искомая точка"""
+folium.PolyLine([geo, geo_objekt(geo,azimut)]).add_to(m)
 
 
 """можно смотреть координаты тыкая по карте"""
--- test.py
+++ test.py
@@ -1,42 +1,54 @@
 from turtle import color
 import folium
 import math
+from folium.plugins import Draw, DualMap, MarkerCluster
 
-m = folium.Map(location=[37.0431575, -7.8449655], zoom_start=14)
+# m = folium.Map(location=[37.0431575, -7.8449655], zoom_start=14)
 
-tootip = "Célula em Olhão"
+# tootip = "Célula em Olhão"
 
-origin_point = [37.040893, -7.83197]
+# origin_point = [37.040893, -7.83197]
 
-folium.CircleMarker(
-    location=origin_point,
-    radius=4,
-    popup="célula",
+# folium.CircleMarker(
+#     location=origin_point,
+#     radius=4,
+#     popup="célula",
     
+# ).add_to(m)
+
+# length = .01
+# angle = 45
+
+# end_lat = origin_point[0] + length * math.sin(math.radians(angle))
+# end_lon = origin_point[1] + length * math.cos(math.radians(angle))
+
+# folium.PolyLine([origin_point, [end_lat, end_lon]]).add_to(m)
+
+#m.save("test.html")
+
+
+# def test_color_line():
+#     m = Map([22.5, 22.5], zoom_start=3)
+#     color_line = features.ColorLine(
+#         [[0, 0], [0, 45], [45, 45], [45, 0], [0, 0]],
+#         [0, 1, 2, 3],
+#         colormap=['b', 'g', 'y', 'r'],
+#         nb_steps=4,
+#         weight=10,
+#         opacity=1)
+#     m.add_child(color_line)
+#     m._repr_html_()
+    
+    
+
+
+m = folium.Map(location=[56.636720806323076, 47.83301060816586], zoom_start=14)
+Draw(
+    export=True,
+    filename='my_data.geojson',
+    position='topleft',
+    draw_options={'polyline': {'allowIntersection': False}},
+    edit_options={'poly': {'allowIntersection': False}}
 ).add_to(m)
-
-length = .01
-angle = 45
-
-end_lat = origin_point[0] + length * math.sin(math.radians(angle))
-end_lon = origin_point[1] + length * math.cos(math.radians(angle))
-
-folium.PolyLine([origin_point, [end_lat, end_lon]]).add_to(m)
-
 m.save("test.html")
 
-
-def test_color_line():
-    m = Map([22.5, 22.5], zoom_start=3)
-    color_line = features.ColorLine(
-        [[0, 0], [0, 45], [45, 45], [45, 0], [0, 0]],
-        [0, 1, 2, 3],
-        colormap=['b', 'g', 'y', 'r'],
-        nb_steps=4,
-        weight=10,
-        opacity=1)
-    m.add_child(color_line)
-    m._repr_html_()
-    
-    
-test_color_line()
